7f081289e95ac49c3b40a45af7d159c28c68ac68
Tommy Gultiano Hansen <54972963+EWCMF@users.noreply.github.com>
2020-02-29 23:14:04 +0100

    Basic foundation for practice UI added.

---
+++ src/hoektest/testui/Main.java
@@ -0,0 +1,19 @@
+package hoektest.testui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("view/MainWindow.fxml"));
+        stage.setTitle("Afskrivning");
+        stage.setScene(new Scene(root, 270, 400));
+        stage.setResizable(false);
+        stage.show();
+    }
+}
